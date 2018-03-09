e7b96878716b38751f7b06cabff30ca24752143c
Jesse Uchytil <36866977+JesseUchytil@users.noreply.github.com>
2018-03-09 11:37:27 -0600

    Catalog items

---
+++ CatalogViewer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupProject_MediaLibrary
+{
+    /**
+     * Authors:            Jesse Uchytil
+     * Date of creation:   3/9/18
+     * Last Edit:          3/9/18
+     * Title:              CatalogViewer.cs
+     * 
+     * Description:  This object keeps track of what portion of the catalog the user is viewing.  Eventually, it could 
+     * hold search criteria and more. 
+     */
+
+    public class CatalogViewer
+    {
+        public CatalogViewer() { }
+        public int pageNumber { get; set; }
+    }
+}
